be08e21f5604bcbdb96d1c7547695ce5935092b7
Rjack00 <rjackson77@comcast.net>
2024-07-02 18:09:44 -0600

    script.js updated
    code refactored and animation code started to visualize recursion

--- script.js
+++ script.js
@@ -1,45 +1,74 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+const animationData = [];
 
+// decimalToBinary() with recursion
 const decimalToBinary = (input) => {
-    const inputs = [];
-    const quotients = [];
-    const remainders = [];
-  
-    if (input === 0) {
-      result.innerText = "0";
-      return;
-    }
-  
-    while (input > 0) {
-      const quotient = Math.floor(input / 2);
-      const remainder = input % 2;
-  
-      inputs.push(input);
-      quotients.push(quotient);
-      remainders.push(remainder);
-      input = quotient;
-    }
-  
-    console.log("Inputs: ", inputs);
-    console.log("Quotients: ", quotients);
-    console.log("Remainders: ", remainders);
-  
-    result.innerText = remainders.reverse().join("");
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
 };
 
+const showAnimation = () => {
+  setTimeout(() => {
+    console.log("free");
+  }, 500);
+  setTimeout(() => {
+    console.log("Code");
+  }, 1000);
+  setTimeout(() => {
+    console.log("Camp");
+  }, 1500);
+};
+
+//// decimalToBinary() with while() loop
+// const decimalToBinary = (input) => {
+//     const inputs = [];
+//     const quotients = [];
+//     const remainders = [];
+  
+//     if (input === 0) {
+//       result.innerText = "0";
+//       return;
+//     }
+  
+//     while (input > 0) {
+//       const quotient = Math.floor(input / 2);
+//       const remainder = input % 2;
+  
+//       inputs.push(input);
+//       quotients.push(quotient);
+//       remainders.push(remainder);
+//       input = quotient;
+//     }
+  
+//     console.log("Inputs: ", inputs);
+//     console.log("Quotients: ", quotients);
+//     console.log("Remainders: ", remainders);
+  
+//     result.innerText = remainders.reverse().join("");
+// };
+
 const checkUserInput = () => {
+  const inputInt = parseInt(numberInput.value);
     if (
       !numberInput.value ||
-      isNaN(parseInt(numberInput.value)) ||
-      parseInt(numberInput.value) < 0
+      isNaN(inputInt) ||
+      inputInt < 0
     ) {
       alert("Please provide a decimal number greater than or equal to 0");
       return;
     }
+
+    if(inputInt === 5){
+      showAnimation();
+      return;
+    }
   
-    decimalToBinary(parseInt(numberInput.value));
+    result.textContent = decimalToBinary(inputInt);
     numberInput.value = "";
   };
 
